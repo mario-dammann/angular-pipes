9cd17a3efb292810f5330d574fdf092c20222631
Mario Dammann <info@mariodammann.de>
2019-06-20 16:33:06 +0200

    [URL] no longer uses switch statement

--- url.pipe.ts
+++ url.pipe.ts
@@ -11,61 +11,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'url'})
 export class url implements PipeTransform {
   transform(value: string, component: string, parameter: string): string {
-
     var url = new URL(value);
 
-    switch (component) {
-      case 'href':
-        return url.href;
-        break;
-
-      case 'origin':
-        return url.origin;
-        break;
-
-      case 'protocol':
-        return url.protocol;
-        break;
-
-      case 'username':
-        return url.username;
-        break;
-
-      case 'password':
-        return url.password;
-        break;
-
-      case 'hostname':
-        return url.hostname;
-        break;
-
-      case 'host':
-        return url.host;
-        break;
-
-      case 'port':
-        return url.port;
-        break;
-
-      case 'pathname':
-        return url.pathname;
-        break;
-
-      case 'search':
-        return url.search;
-        break;
-
-      case 'searchParams':
-        return url.searchParams.get(parameter);
-        break;
-
-      case 'hash':
-        return url.hash;
-        break;
-
-      default:
-        return url.href;
-        break;
+    if (component in url) {
+      if (component == 'searchParams') {
+        return url[component].get(parameter);
+      }
+      return url[component];
     }
+    return url.href;
   }
 }
